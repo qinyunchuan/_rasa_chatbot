97d1fde2852c6f6cca7520bda4b3c08f4e9077c9
qinyunchuan <yunchuanqin@qq.com>
2018-07-06 22:58:17 +0800

    更新到最新版本rasa

--- bot.py
+++ bot.py
@@ -61,40 +61,67 @@ class ActionSearchConsume(Action):
 
 
 class MobilePolicy(KerasPolicy):
-    def model_architecture(self, num_features, num_actions, max_history_len):
+ def model_architecture(self, input_shape, output_shape):
         """Build a Keras model and return a compiled model."""
         from keras.layers import LSTM, Activation, Masking, Dense
         from keras.models import Sequential
 
-        n_hidden = 32  # size of hidden layer in LSTM
+        from keras.models import Sequential
+        from keras.layers import \
+            Masking, LSTM, Dense, TimeDistributed, Activation
+
         # Build Model
-        batch_shape = (None, max_history_len, num_features)
-
         model = Sequential()
-        model.add(Masking(-1, batch_input_shape=batch_shape))
-        model.add(LSTM(n_hidden, batch_input_shape=batch_shape))
-        model.add(Dense(input_dim=n_hidden, output_dim=num_actions))
-        model.add(Activation("softmax"))
 
-        model.compile(loss="categorical_crossentropy",
-                      optimizer="adam",
-                      metrics=["accuracy"])
+        # the shape of the y vector of the labels,
+        # determines which output from rnn will be used
+        # to calculate the loss
+        if len(output_shape) == 1:
+            # y is (num examples, num features) so
+            # only the last output from the rnn is used to
+            # calculate the loss
+            model.add(Masking(mask_value=-1, input_shape=input_shape))
+            model.add(LSTM(self.rnn_size))
+            model.add(Dense(input_dim=self.rnn_size, units=output_shape[-1]))
+        elif len(output_shape) == 2:
+            # y is (num examples, max_dialogue_len, num features) so
+            # all the outputs from the rnn are used to
+            # calculate the loss, therefore a sequence is returned and
+            # time distributed layer is used
+
+            # the first value in input_shape is max dialogue_len,
+            # it is set to None, to allow dynamic_rnn creation
+            # during prediction
+            model.add(Masking(mask_value=-1,
+                              input_shape=(None, input_shape[1])))
+            model.add(LSTM(self.rnn_size, return_sequences=True))
+            model.add(TimeDistributed(Dense(units=output_shape[-1])))
+        else:
+            raise ValueError("Cannot construct the model because"
+                             "length of output_shape = {} "
+                             "should be 1 or 2."
+                             "".format(len(output_shape)))
+
+        model.add(Activation('softmax'))
+
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
 
         logger.debug(model.summary())
         return model
-
 
 def train_dialogue(domain_file="mobile_domain.yml",
                    model_path="models/dialogue",
                    training_data_file="data/mobile_story.md"):
     agent = Agent(domain_file,
-                  policies=[MemoizationPolicy(), MobilePolicy()])
+                  policies=[MemoizationPolicy(max_history=3), MobilePolicy()])
 
+    training_data = agent.load_data(training_data_file);
     agent.train(
-        training_data_file,
-        max_history=2,
-        epochs=200,
-        batch_size=16,
+        training_data,
+        epochs=400,
+        batch_size=100,
         augmentation_factor=50,
         validation_split=0.2
     )
@@ -117,7 +144,7 @@ def train_nlu():
 
 
 def run_ivrbot_online(input_channel=ConsoleInputChannel(),
-                      interpreter=RasaNLUInterpreter("models/ivr/demo"),
+                      interpreter=RasaNLUInterpreter("models/default/model_20180706-192633"),
                       domain_file="mobile_domain.yml",
                       training_data_file="data/mobile_story.md"):
     agent = Agent(domain_file,
@@ -136,7 +163,7 @@ def run_ivrbot_online(input_channel=ConsoleInputChannel(),
 
 def run(serve_forever=True):
     agent = Agent.load("models/dialogue",
-                       interpreter=RasaNLUInterpreter("models/ivr/demo"))
+                       interpreter=RasaNLUInterpreter("models/default/model_20180706-192633"))
 
     if serve_forever:
         agent.handle_channel(ConsoleInputChannel())
